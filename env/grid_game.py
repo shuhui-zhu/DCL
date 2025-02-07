28cf339091da137410d8e987b0609a54cf44ad76
Sophiakkk <zhuzhuhsm@gmail.com>
2025-02-07 17:11:49 -0500

    initial commit

---
+++ env/grid_game.py
@@ -0,0 +1,69 @@
+import numpy as np
+
+class GridSocialDilemmaEnv: 
+    NAME = 'GridGame'
+    NUM_AGENTS = 2
+    NUM_ACTIONS = 2
+    
+    def __init__(self, max_steps=16, grid_size=4, k=2, num_agents = 2, num_actions = 2):
+        super(GridSocialDilemmaEnv, self).__init__()
+        self.max_steps = max_steps
+        self.grid_size = grid_size
+        self.num_agents = num_agents
+        self.num_actions = num_actions
+        self.conflict_level = k
+
+        self.initial_position_agent_0 = np.identity(self.grid_size)[0]
+        self.initial_position_agent_1 = np.identity(self.grid_size)[-1]
+        self.initial_state = np.concatenate([self.initial_position_agent_0, self.initial_position_agent_1],axis=0).reshape(1,-1)
+        self.agent_positions = [0, self.grid_size - 1]
+
+    def reset(self):
+        self.current_step = 0
+        self.agent_positions = [0, self.grid_size - 1]
+        state = self.initial_state
+        return state
+
+    def step(self, actions):
+        self.current_step += 1
+        
+        for i in range(self.num_agents):
+            if actions[i]==0: # stay
+                if self.agent_positions[i] > 0:
+                    self.agent_positions[i] -= 1
+                elif self.agent_positions[i]==0:
+                    self.agent_positions[i] = 0
+                else:
+                    AssertionError("Negative position")
+            elif actions[i]==1:
+                if self.agent_positions[i] < self.grid_size - 1:
+                    self.agent_positions[i] += 1
+                elif self.agent_positions[i]==self.grid_size-1:
+                    self.agent_positions[i] = self.grid_size - 1
+                else:
+                    AssertionError("Over the grid size")
+        
+        # reveal agents' positions
+        state = np.concatenate([np.identity(self.grid_size)[self.agent_positions[0]], np.identity(self.grid_size)[self.agent_positions[1]]],axis=0).reshape(1,-1)            
+        immediate_rewards = self.calculate_rewards()
+        
+        done = (self.current_step == self.max_steps)
+        
+        return state, immediate_rewards, done
+
+    def calculate_rewards(self):
+        pos_0, pos_1 = self.agent_positions
+        reward_0 = pos_0 - self.conflict_level*(self.grid_size-1 - pos_1)
+        reward_1 = self.grid_size-1 - pos_1 - self.conflict_level*pos_0
+        rewards = np.array([reward_0, reward_1])
+        return rewards
+
+    def render(self):
+        grid = [' ' for _ in range(self.grid_size)]
+        if self.agent_positions[0] == self.agent_positions[1]:
+            grid[self.agent_positions[0]] = 'AB'
+        else:
+            grid[self.agent_positions[0]] = 'A'
+            grid[self.agent_positions[1]] = 'B'
+        print('|'.join(grid))
+        print(f"Agent A Position: {self.agent_positions[0]}, Agent B Position: {self.agent_positions[1]}")
---
+++ env/repeated_game.py
@@ -0,0 +1,59 @@
+import numpy as np
+
+class IteratedPrisonersDilemma:
+    """
+    A two-agent vectorized environment for the Prisoner's Dilemma game.
+    Possible actions for each agent are (C)ooperate and (D)efect.
+    """
+    NAME = 'IPD'
+    NUM_AGENTS = 2
+    NUM_ACTIONS = 2
+    NUM_STATES = 1 
+
+    def __init__(self, max_steps):
+        self.max_steps = max_steps
+        self.payout_mat = np.array([[-1., 0.], [-3., -2.]])
+        self.dummy_state = np.array([[1., 0.]])
+
+    def reset(self):
+        self.step_count = 0
+        return self.dummy_state
+
+    def step(self, actions):
+        ac0, ac1 = actions
+        self.step_count += 1
+
+        rewards = np.array([self.payout_mat[ac1][ac0], self.payout_mat[ac0][ac1]])
+
+        done = (self.step_count == self.max_steps)
+
+        return self.dummy_state, rewards, done
+
+class IteratedPureConflict:
+    """
+    A two-agent vectorized environment for a pure-conflicting interests game.
+    Possible actions for each agent are (C)ooperate and (D)efect.
+    """
+    NAME = 'IPC'
+    NUM_AGENTS = 2
+    NUM_ACTIONS = 2
+    NUM_STATES = 1 
+
+    def __init__(self, max_steps):
+        self.max_steps = max_steps
+        self.payout_mat = np.array([[0., 2.], [-1., 0.]])
+        self.dummy_state = np.array([[1., 0.]])
+
+    def reset(self):
+        self.step_count = 0
+        return self.dummy_state
+
+    def step(self, actions):
+        ac0, ac1 = actions
+        self.step_count += 1
+
+        rewards = np.array([self.payout_mat[ac1][ac0], self.payout_mat[ac0][ac1]])
+
+        done = (self.step_count == self.max_steps)
+
+        return self.dummy_state, rewards, done
